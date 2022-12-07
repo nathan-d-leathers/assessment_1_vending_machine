3dddff8fe1f26e3c1efa1b063709e196e6566df5
Nathan-D-Leathers <nathan.d.leathers@icloud.com>
2022-12-07 13:46:01 -0800

    checking functionality through unit testing

--- improved_vending_machine.py
+++ improved_vending_machine.py
@@ -1,21 +1,19 @@
-# PseudoCode Notes:
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Assessment #1:
 
-# Start at $100.00 and work backwards to $0.00
-# Create a dictionary that defines what each denomiation of currency represents
-# Use numbers for math and strings for reply message.
-# Keep track of the number of times a bill or coin is added to total change due
-# Make sure plural instances of denominations are accounted for
+# Create a vending machine that returns correct change for items up to $100
 
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 import itertools
-# used later to merge lists
 
 
 def optimal_change(item_cost, amount_paid):
-
     total_change = []
     num_of_bills_and_coins = []
     verbage = []
+    answer = []
+
     num = 0
     currency = {
         10000: '$100',
@@ -29,25 +27,19 @@ def optimal_change(item_cost, amount_paid):
         5: '$.05',
         1: '$.01',
     }
-    # dictionary of all possible change denominations
 
     total_due = round(amount_paid * 100 - item_cost * 100)
-    # total change due in pennies (used to simplify decimal math)
     if total_due == 0:
-        return(print("No Change Due"))
+        return("No Change Due")
     elif total_due < 0:
-        return(print("Insert More Cash To Recieve Item"))
+        return("Insert More Cash To Recieve Item")
     else:
         for change in currency:
             num = 0
             while int(total_due) >= int(change):
-                # loop accesed so long as currency denomination can be subtracted from total change due
-                # while loop utilizied to handle multiple occurences of the same denomination
                 if num == 0:
-                    # this loop is accesed the first time a change denomination is required
                     if total_due > 100:
                         total_change.append(currency[change])
-                    # included this if statement to not add coin values to final output
                     num += 1
                     total_due -= int(change)
                     bill = "bill"
@@ -56,8 +48,6 @@ def optimal_change(item_cost, amount_paid):
                     nickel = "nickel"
                     penny = "penny"
                 else:
-                    # this loop accesed for multiple instances of the same change denomination
-                    # this loop also changes my verbage for plural situations
                     num += 1
                     total_due -= int(change)
                     bill = "bills"
@@ -66,9 +56,7 @@ def optimal_change(item_cost, amount_paid):
                     nickel = "nickels"
                     penny = "pennies"
             if num > 0:
-                # used to keep track of required number of each change denomation
                 num_of_bills_and_coins.append(str(num))
-                # if/elif/else statements used to keep track of verbage variable
                 if int(change) >= 100:
                     verbage.append(str(bill))
                 elif int(change) >= 25:
@@ -80,14 +68,28 @@ def optimal_change(item_cost, amount_paid):
                 else:
                     verbage.append(str(penny))
 
-    # At this point there are three seperate sequential lists for:
-    # 1: Number of each change denomiation
-    # 2: Change type
-    # 3: Change verbage
-    # First the function prints the general return statement
-    # Next, the three lits are merged together with itertools and printed to the screen.
+    price = item_cost
+    payment = amount_paid
+    optimal_statement = "The optimal change for an item that costs $" + \
+        str(f"{price:.2f} ") + "with an amount paid of $" + \
+        str(f"{payment:.2f} ") + "is "
+    a_tracker = 0
 
-    print(
-        f"The optimal change for an item that costs ${str(item_cost)} with an amount paid of ${str(amount_paid)} is: ")
     for (a, b, c) in itertools.zip_longest(num_of_bills_and_coins, total_change, verbage, fillvalue=""):
-        print(a, b, c)
+        if (a_tracker == len(num_of_bills_and_coins) - 1) and (len(num_of_bills_and_coins) != 1):
+            answer.append("and " + a)
+        else:
+            answer.append(a)
+            a_tracker += 1
+        if b != "":
+            answer.append(b)
+        answer.append(c + ",")
+    answer[-1] = c + "."
+    change_due = " ".join(answer)
+    return(optimal_statement + change_due)
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-
+
+# All Bugs Fixed!!!
+
+# -=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-==-=-=-=-=-=-=--=-=-=-=-=-=-
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,27 +1,27 @@
 # Write your tests here!
-from optimal_change import opitmal_change
+from improved_vending_machine import optimal_change
 
 # The first varriable is the cost of the item, the second variable is the payment received
 
-opitmal_change(100.00, 50.00)
+print(optimal_change(100.00, 50.00))
 # expected output:
 # Insert More Cash To Recieve Item
 
-opitmal_change(100.00, 100.00)
+print(optimal_change(100.00, 100.00))
 # expected output:
 # No Change Due
 
-opitmal_change(50.00, 100.00)
+print(optimal_change(50.00, 100.00))
 # expected output:
 # The optimal change for an item that costs 50.0 with an amount paid of 100.0 is:
 # 1 $50 bill
 
-opitmal_change(99.99, 100.00)
+print(optimal_change(99.99, 100.00))
 # expected output:
 # The optimal change for an item that costs 99.99 with an amount paid of 100.0 is:
 # 1  penny
 
-opitmal_change(1.00, 100.00)
+print(optimal_change(1.00, 100.00))
 # expected output:
 # The optimal change for an item that costs 1.0 with an amount paid of 100.0 is:
 # 1 $50 bill
@@ -29,7 +29,7 @@ opitmal_change(1.00, 100.00)
 # 1 $5 bill
 # 4 $1 bills
 
-opitmal_change(.01, 100.00)
+print(optimal_change(.01, 100.00))
 # expected output:
 # The optimal change for an item that costs 0.01 with an amount paid of 100.0 is:
 # 1 $50 bill
@@ -39,3 +39,11 @@ opitmal_change(.01, 100.00)
 # 3  quarters
 # 2  dimes
 # 4  pennies
+
+print(optimal_change(0.01, 0.92))
+# output:
+# The optimal change for an item that costs $0.01 with an amount paid of $0.92 is:
+# 3 quarters
+# 1 dime
+# 1 nickel
+# 1 penny
